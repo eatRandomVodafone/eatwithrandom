992d987616b5b2d14e21480eed83c3358e9c6fd8
Alberto Mozo <alberto.mozo@vodafone.com>
2019-07-10 16:57:27 +0200

    .

--- src/main/java/com/vodafone/eatwithrandom/repository/PoolGrupalRepository.java
+++ src/main/java/com/vodafone/eatwithrandom/repository/PoolGrupalRepository.java
@@ -1,15 +1,31 @@
 package com.vodafone.eatwithrandom.repository;
 
+import java.util.Optional;
+
 import com.vodafone.eatwithrandom.model.PoolGrupal;
 import com.vodafone.eatwithrandom.model.User;
 
-import java.util.List;
-import java.util.Optional;
-
 public interface PoolGrupalRepository{
 
+	/**
+	 * 
+	 * @param userGroup
+	 * @return
+	 */
 	public User saveUserPoolGroup(PoolGrupal userGroup);
     
+	/**
+	 * 
+	 * @param userId
+	 */
     public void deleteUserPoolGroup(String userId);
+    
+    /**
+     * 
+     * @param search
+     * @param field
+     * @return
+     */
+    public Optional<User> findBy(String search, String field);
     
 }
--- src/main/java/com/vodafone/eatwithrandom/repository/PoolGrupalRepositoryImpl.java
+++ src/main/java/com/vodafone/eatwithrandom/repository/PoolGrupalRepositoryImpl.java
@@ -10,7 +10,6 @@ import org.springframework.data.mongodb.core.MongoOperations;
 import org.springframework.stereotype.Repository;
 import org.springframework.util.Assert;
 
-import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.mongodb.core.query.Criteria;
