05cc21363e6620ebca0ad0f480f95e7300a954ed
Alberto Mozo <alberto.mozo@vodafone.com>
2019-07-11 10:36:56 +0200

    Arreglos varios

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
 FROM openjdk:8-jdk-alpine
-ADD eatwithrandom-1.0.0.jar app.jar
+ADD eatwithrandom-1.0.1.jar app.jar
 EXPOSE 4444
 ENTRYPOINT ["java","-Djava.security.egd=file:/dev/./urandom","-jar","/app.jar"]
--- src/main/java/com/vodafone/eatwithrandom/controller/UserController.java
+++ src/main/java/com/vodafone/eatwithrandom/controller/UserController.java
@@ -6,6 +6,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.web.DefaultRedirectStrategy;
 import org.springframework.security.web.RedirectStrategy;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -31,6 +32,7 @@ public class UserController {
 
     private RedirectStrategy redirectStrategy = new DefaultRedirectStrategy();
 
+    @CrossOrigin
     @PostMapping("/signin")
     public UserResponseDTO login(
     		@RequestBody Login login) {
